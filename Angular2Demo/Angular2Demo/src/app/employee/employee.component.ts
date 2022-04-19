89e7e92d47cdba8bc2d9037cbc61c6b7b70df58f
vsha041 <varun.varunsharma@gmail.com>
2022-04-19 17:57:51 +1200

    Part - 7 - Styling Angular 2 components

--- Angular2Demo/Angular2Demo/src/app/employee/employee.component.ts
+++ Angular2Demo/Angular2Demo/src/app/employee/employee.component.ts
@@ -2,7 +2,8 @@
 
 @Component({
 	selector: 'my-employee',
-	templateUrl: 'app/employee/employee.component.html'
+	templateUrl: 'app/employee/employee.component.html',
+	styleUrls: ['app/employee/employee.component.css']
 })
 export class EmployeeComponent {
 	firstName: string = "Tom";
