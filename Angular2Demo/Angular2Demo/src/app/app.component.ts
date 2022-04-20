884f9922e42b46fcc35910633c3f14deab4c082d
vsha041 <varun.varunsharma@gmail.com>
2022-04-20 21:29:36 +1200

    Part 8 - Angular Interpolation

--- Angular2Demo/Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/Angular2Demo/src/app/app.component.ts
@@ -6,4 +6,12 @@ import {Component} from "@angular/core"
 })
 export class AppComponent {
     pageHeader: string = "Employee Details";
+    imagePath: string = "https://www.pragimtech.com/wp-content/uploads/2019/02/pargim-logo-1.png";
+
+    firstName: string = 'Tom';
+    lastName: string = 'Hopkins';
+
+    getFullName(): string {
+        return this.firstName + " " + this.lastName;
+    }
 }
