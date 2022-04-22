c8b953b1653ef82133aaf25e3f60c8da057adfb6
vsha041 <varun.varunsharma@gmail.com>
2022-04-23 09:25:14 +1200

    Part - 14 - Angular2 Event Binding

--- Angular2Demo/Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/Angular2Demo/src/app/app.component.ts
@@ -5,17 +5,7 @@ import {Component} from "@angular/core"
     templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
-    isBold: boolean = true;
-    fontSize: number = 50;
-    isItalic: boolean = true;
-
-    addStyles() {
-        let styles = {
-            'font-size.px': this.fontSize,
-            'font-style': this.isItalic ? 'italic' : 'normal',
-            'font-weight': this.isBold ? 'bold' : 'normal'
-        }
-
-        return styles;
+    onClick() {
+        console.log("button clicked");
     }
 }
