03a1aa26b87e484d0d6787f0e9ae54d8dcd25d1c
vsha041 <varun.varunsharma@gmail.com>
2022-04-24 17:29:37 +1200

    Part 16 - Angular ngFor directive

--- Angular2Demo/Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/Angular2Demo/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { EmployeeComponent }  from "./employee/employee.component";
+import { EmployeeListComponent }  from "./employee/employeeList.component";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, EmployeeComponent ],
+  declarations: [ AppComponent, EmployeeComponent, EmployeeListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls: ['app/employee/employeeList.component.css']
+})
+export class EmployeeListComponent {
+    employees: any[] = [
+        
+    ];
+}
