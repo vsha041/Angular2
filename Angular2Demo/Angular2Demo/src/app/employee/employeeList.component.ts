2091e9d2f4ae8a117df77b7101e76fb90665b191
vsha041 <varun.varunsharma@gmail.com>
2022-04-25 08:55:11 +1200

    Part 18 - Angular pipes

--- Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
@@ -12,7 +12,7 @@ export class EmployeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '6/25/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -24,7 +24,7 @@ export class EmployeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '10/14/1980'
             }
         ];
     }
