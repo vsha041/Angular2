02ebc135b7a79aa0de70ccb20dc83e522e27a5df
vsha041 <varun.varunsharma@gmail.com>
2022-04-28 08:34:55 +1200

    Part 19 - Angular custom pipe

--- Angular2Demo/Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/Angular2Demo/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { EmployeeComponent }  from "./employee/employee.component";
-import { EmployeeListComponent }  from "./employee/employeeList.component";
+import { EmployeeListComponent } from "./employee/employeeList.component";
+import { EmployeeTitlePipe } from "./employee/employeeTitle.pipe";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, EmployeeComponent, EmployeeListComponent ],
+    declarations: [AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
