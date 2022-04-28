d1be82d4de2139659284a9c69f556a342ed0a615
vsha041 <varun.varunsharma@gmail.com>
2022-04-28 22:25:33 +1200

    Part 20 - Angular 2 container and nested components

--- Angular2Demo/Angular2Demo/src/app/app.module.ts
+++ Angular2Demo/Angular2Demo/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent }  from './app.component';
 import { EmployeeComponent }  from "./employee/employee.component";
 import { EmployeeListComponent } from "./employee/employeeList.component";
 import { EmployeeTitlePipe } from "./employee/employeeTitle.pipe";
+import { EmployeeCountComponent } from "./employee/employeeCount.component";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-    declarations: [AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe ],
+  declarations: [AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe, EmployeeCountComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeCount.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'employee-count',
+    templateUrl: 'app/employee/employeeCount.component.html',
+    styleUrls: ['app/employee/employeeCount.component.css']
+})
+export class EmployeeCountComponent {
+    all: number = 10;
+    male: number = 5;
+    female: number = 5;
+}
